e911a9eb8e888de01cc8165a63aebea008c79540
mimowR <83735226+mimowR@users.noreply.github.com>
2021-05-19 10:57:41 -0500

    Update cert.ps1

--- cert.ps1
+++ cert.ps1
@@ -135,8 +135,7 @@ Start-Sleep -s 5
 
 $files = Get-ChildItem -Path $Destination
 
-For($i = 1; $i -le $files.count; $i++)
-
+#For($i = 1; $i -le $files.count; $i++)
 #{ Write-Progress -Activity "“Collecting files” -status “Finding file $i”" -percentComplete ($i / $files.count*100)}
 
 $files | Select name
