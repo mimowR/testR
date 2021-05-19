3fa3fae1bf555887261952f438938ded908eab63
mimowR <83735226+mimowR@users.noreply.github.com>
2021-05-19 10:36:40 -0500

    Update cert.ps1

--- cert.ps1
+++ cert.ps1
@@ -137,7 +137,7 @@ $files = Get-ChildItem -Path $Destination
 
 For($i = 1; $i -le $files.count; $i++)
 
-{ Write-Progress -Activity "“Collecting files” -status “Finding file $i”" -percentComplete ($i / $files.count*100)}
+#{ Write-Progress -Activity "“Collecting files” -status “Finding file $i”" -percentComplete ($i / $files.count*100)}
 
 $files | Select name
 Write-Host "[+] Files copied" -ForegroundColor Green
